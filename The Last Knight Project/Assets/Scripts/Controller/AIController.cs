4440b5676eeea18f3e2a3c4a22a67534ccfaa4f2
Dabronzo <ayrtonterceiro@gmail.com>
2022-04-29 11:47:38 +0200

    Implemented suspicious behaviour

--- The Last Knight Project/Assets/Scripts/Controller/AIController.cs
+++ The Last Knight Project/Assets/Scripts/Controller/AIController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using RPG.Combat;
 using RPG.GameCore;
+using RPG.Movements;
 
 namespace RPG.Controller
 {
@@ -10,16 +11,30 @@ namespace RPG.Controller
     {
         //Varible of chaise distance
         [SerializeField] float chaiseDistance = 5f;
+        [SerializeField] float suspiciusTime = 5f;
 
         Fighter fighterComponent;
         Health health;
         GameObject player;
+        Mover mover;
+
+        //to make some sort of state where the AI can do some behaivours
+        //The guard behaivour is just when the npc remembers the initial position
+        //and after the chaise will return to guard there
+        Vector3 guardingPosition;
+
+        //to make the suspicius behaivour we gonna set a varible to the npc remembers when was the last
+        //time that the player was in his interest area
+        float timeSinceLastSawPlayer = Mathf.Infinity;
 
         private void Start()
         {
             fighterComponent = GetComponent<Fighter>();
             health = GetComponent<Health>();
             player = GameObject.FindWithTag("Player");
+            mover = GetComponent<Mover>();
+
+            guardingPosition = transform.position;
         }
 
 
@@ -30,21 +45,54 @@ namespace RPG.Controller
             
             if(InRangeChaise() && fighterComponent.CanAttack(player))
             {
-               fighterComponent.Attack(player);
-                
+                timeSinceLastSawPlayer = 0;
+                AttackBehaivour();
+
+            }
+            //to check the suspicius behaivour
+            else if (timeSinceLastSawPlayer < suspiciusTime)
+            {
+                SuspiciusBehauvour();
+
             }
             else
             {
-                fighterComponent.Cancel();
+                GuardingBehaivour();
             }
+            //to update the timeSinceLastSawPlayer
+            timeSinceLastSawPlayer += Time.deltaTime;
         }
 
+        private void GuardingBehaivour()
+        {
+            //fighterComponent.Cancel();
+            //since the StartMoveAction calls the action scheduler that will cancel
+            //the previous behave so don't need to cancel the attack
+            mover.StartMoveAction(guardingPosition);
+        }
 
+        private void SuspiciusBehauvour()
+        {
+            GetComponent<ActionScheduler>().CancelCurrentAction();
+        }
+
+        private void AttackBehaivour()
+        {
+            fighterComponent.Attack(player);
+        }
 
         public bool InRangeChaise()
         {
             //calculate the distance and returning a bool
             return Vector3.Distance(player.transform.position, transform.position) <= chaiseDistance;
+        }
+
+        //Draw Gizmos method called by Unity to visualize the chaise are of the NPC
+        private void OnDrawGizmosSelected() 
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(transform.position, chaiseDistance);
+            
         }
     
     }
