9541a64785fad27190ee1381f6ca1cd4ba10f8d2
Dabronzo <ayrtonterceiro@gmail.com>
2022-04-29 20:31:52 +0200

    Created first cutscenes structure

---
+++ The Last Knight Project/Assets/Scripts/Cinematics/CinematicTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematics
+{
+    public class CinematicTrigger : MonoBehaviour
+    {
+        bool wasTriggered = false;
+
+        private void OnTriggerEnter(Collider other) 
+        {
+            if (!wasTriggered && other.gameObject.tag == "Player")
+            {
+                wasTriggered = true;
+                GetComponent<PlayableDirector>().Play();  
+            }
+            
+            
+        }
+
+    }
+    
+}
